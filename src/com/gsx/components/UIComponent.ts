f1ac76bbca94fa572f122184d7b29e341c31516b
kuanghongrui <kuanghongrui@baijiahulian.com>
2015-10-27 23:55:14 +0800

    Add event handlers

--- src/com/gsx/components/UIComponent.ts
+++ src/com/gsx/components/UIComponent.ts
@@ -21,6 +21,32 @@ export class UIComponent extends EventDispatcher implements IBox, ITemplated, IC
     constructor(params?: Object) {
         super();
         this.create(params);
+        this.listenerEvents();
+    }
+
+    /**
+     * To listener events.
+     */
+    protected listenerEvents() {
+        var node:Node = this.getNode();
+        node.addEventListener('focus', this.emit.bind(this, 'focus'));
+        node.addEventListener('blur', this.emit.bind(this, 'blur'));
+        node.addEventListener('contextmenu', this.emit.bind(this, 'contextmenu'));
+        node.addEventListener('resize', this.emit.bind(this, 'resize'));
+
+        node.addEventListener('keydown', this.emit.bind(this, 'keydown'));
+        node.addEventListener('keypress', this.emit.bind(this, 'keypress'));
+        node.addEventListener('keyup', this.emit.bind(this, 'keyup'));
+
+        node.addEventListener('click', this.emit.bind(this, 'click'));
+        node.addEventListener('dbclick', this.emit.bind(this, 'dbclick'));
+        node.addEventListener('mousedown', this.emit.bind(this, 'mousedown'));
+        node.addEventListener('mouseup', this.emit.bind(this, 'mouseup'));
+        node.addEventListener('mousemove', this.emit.bind(this, 'mousemove'));
+        node.addEventListener('mouseover', this.emit.bind(this, 'mouseover'));
+        node.addEventListener('mouseout', this.emit.bind(this, 'mouseout'));
+        node.addEventListener('mousewheel', this.emit.bind(this, 'mousewheel'));
+        node.addEventListener('scroll', this.emit.bind(this, 'scroll'));
     }
 
     /**
--- src/com/gsx/components/form/FormComponent.ts
+++ src/com/gsx/components/form/FormComponent.ts
@@ -9,11 +9,43 @@ import {SkinableComponent} from 'com/gsx/components/SkinableComponent';
 export class FormComponent extends SkinableComponent {
 
     /**
+     * The name of the form component.
+     */
+    private name:string = '';
+
+    /**
+     * The value of the form component.
+     */
+    private value:Object;
+
+    constructor() {
+        super();
+        var node:Node = this.getNode();
+        node.addEventListener('change', this.emit.bind(this, 'change'));
+    }
+
+    /**
      * Get the status of disabled.
      * @return {boolean} Whether disabled or not。
      */
     public getDisabled(): boolean {
         return (<Element>this.getNode()).hasAttribute('disabled');
+    }
+
+    /**
+     * Get the name of the form component.
+     * @return {string} The name of the form component.
+     */
+    public getName(): string {
+        return this.name;
+    }
+
+    /**
+     * Get the value of the form component.
+     * @return {string} The value of the form component.
+     */
+    public getValue(): Object {
+        return this.value;
     }
 
     /**
@@ -28,5 +60,21 @@ export class FormComponent extends SkinableComponent {
         } else {
             (<Element>this.getNode()).removeAttribute('disabled');
         }
+    }
+
+    /**
+     * Set the name of the form component.
+     * @param {string} name The name of the form component.
+     */
+    public setName(name: string): void {
+        this.name = name;
+    }
+
+    /**
+     * Set the value of the form component.
+     * @param {string} value The value of the form component.
+     */
+    public setValue(value: Object): void {
+        this.value = value;
     }
 }
