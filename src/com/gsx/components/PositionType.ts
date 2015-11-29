488a5679ba842a420d1d2de899035401d51265d2
kuanghongrui <kuanghongrui@baijiahulian.com>
2015-11-30 00:02:40 +0800

    Add enum postion type

---
+++ src/com/gsx/components/PositionType.ts
@@ -0,0 +1,17 @@
+/**
+ * @file The Position Type.
+ *
+ * @author kuanghongrui@baijiahulian.com
+ */
+
+export enum PositionType {
+    TOP, // top position
+    RIGHT, // right position
+    BOTTOM, // bottom position
+    LEFT, // left position
+
+    TOP_LEFT, // top left postion
+    TOP_RIGHT, // top right postion
+    BOTTOM_LEFT, // bottom left potion
+    BOTTOM_RIGHT // bottom right position
+}
